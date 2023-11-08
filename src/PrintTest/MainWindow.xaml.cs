02d97c07aff80da9bf38b92d80591bdccbff7dff
liu <liu@huawei>
2023-11-08 18:50:03 +0800

    print with PrintDocument

--- src/PrintTest/MainWindow.xaml.cs
+++ src/PrintTest/MainWindow.xaml.cs
@@ -1,21 +1,61 @@
-using System.Windows.Controls;
+using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace PrintTest;
 
 public partial class MainWindow
 {
+    private readonly PrintDocument _document;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        _document = new PrintDocument();
+        _document.PrinterSettings.PrinterName = "Microsoft Print to PDF";
+        _document.PrinterSettings.PrintToFile = true;
+        _document.PrintPage += PrintPage;
     }
 
-    private void OnPrintClick(object sender, System.Windows.RoutedEventArgs e)
+    private void PrintPage(object sender, PrintPageEventArgs e)
     {
-        var printDialog = new PrintDialog();
+        var source = Canvas.FrameworkElementToBitmapSource();
+        var encoder = new BmpBitmapEncoder();
+        encoder.Frames.Add(BitmapFrame.Create(source));
+        using var ms = new MemoryStream();
+        encoder.Save(ms);
+        var image = Image.FromStream(ms);
 
-        if (printDialog.ShowDialog() == true)
+        e.Graphics?.DrawImage(image, new System.Drawing.Point(0, 0));
+    }
+
+    private void OnPrintClick(object sender, RoutedEventArgs e)
+    {
+        _document.PrinterSettings.PrintFileName = $"pdf_{DateTime.Now:yy-MM-dd_HH-mm-ss_fff}.pdf";
+
+        _document.Print();
+    }
+}
+
+public static class FrameworkElementExtensions
+{
+    public static BitmapSource FrameworkElementToBitmapSource(this FrameworkElement element)
+    {
+        element.UpdateLayout();
+        var width = element.ActualWidth;
+        var height = element.ActualHeight;
+        var dv = new DrawingVisual();
+        using (var dc = dv.RenderOpen())
         {
-            printDialog.PrintVisual(Canvas, "A Simple Drawing");
+            dc.DrawRectangle(new BitmapCacheBrush(element), null, new Rect(0, 0, width, height));
         }
+        var rtb = new RenderTargetBitmap((int)width, (int)height, 96d, 96d, PixelFormats.Pbgra32);
+        rtb.Render(dv);
+        return rtb;
     }
 }
